e9b9cd4d23530177674b0ee981aeaad83eaa92f0
Or Abuhtzura <44296674+orabu103@users.noreply.github.com>
2019-05-09 23:12:30 +0300

    Update range.hpp

--- range.hpp
+++ range.hpp
@@ -1,8 +1,26 @@
 #pragma once
 using namespace std;
 #include <iostream>
-template<typename T>
-class range{
 
 
-};
+namespace itertools{
+    template<typename T>
+    class range{
+    
+        public:
+        T _begin;
+        T _end;
+
+        range(const T& begin,const T& end): _begin(begin), _end(end) {}
+        
+        struct iterator {
+            T type;  
+            iterator(const T& p) :type(p) {} 
+            iterator& operator++(){ ++type; return *this; } 
+            T& operator*() { return type; } 
+            bool operator!=(const iterator& iter) { return type != iter.type; }  
+        };
+        iterator begin(){ return _begin; }
+        iterator end(){ return _end; }
+    };
+}
