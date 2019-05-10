0ee2c49e8455fcb24a04d55c8d2d2e970c3638ca
Or Abuhtzura <44296674+orabu103@users.noreply.github.com>
2019-05-10 12:40:47 +0300

    Update zip.hpp

--- zip.hpp
+++ zip.hpp
@@ -1,71 +1,65 @@
-#ifndef ZIP_HPP
-#define ZIP_HPP
+#pragma once
+using namespace std;
 
 #include <iostream>
+ 
+namespace itertools{
+    template<typename T , typename U>
 
-namespace itertools {
-
-template <typename T1, typename T2> class zip {
-        private:
-        T1 a; // range(5,8) for Example
-        T2 b; // range(9,10) for Example
-        
+    class zip{
         public:
-        zip(const T1& r1,const T2& r2) : a(r1), b(r2) {}
+        T _TypeA;
+        U _TypeB;
 
-    class iterator {  
-      template <typename V, typename K>
-      class MyPair {
-      V v;
-      K k;
-      public:
-      MyPair(const V& v, const K& k) : v(v), k(k) {}
-      MyPair() {}
+        zip(const T& TypeA , const U& TypeB): _TypeA(TypeA) , _TypeB(TypeB) {}
 
-      friend std::ostream& operator<<(std::ostream& os,const MyPair<V,K>& pair)
-      {
-        return os << pair.v << "," << pair.k;
-      }
-      }; // End Class MyPair
+        class iterator {
+            template <typename V,typename K >
+            class MyPair {
+                V v;
+                K k;
+                public:
+                MyPair(const V& v, const K& k) : v(v), k(k) {}
+                MyPair() {}
 
-        decltype(a.begin()) t1_begin;
-        decltype(a.end()) t1_end;
+                friend std::ostream& operator<<(std::ostream& os,const MyPair<V,K>& pair)
+                {
+                    return os << pair.v << "," << pair.k;
+                }
+            }; // End Class MyPair
 
-        decltype(b.begin()) t2_begin;
-        decltype(b.end()) t2_end;
-       
-        public:      
-        iterator(T1& a, T2& b) 
-        : t1_begin(a.begin()), t1_end(a.end()),
-          t2_begin(b.begin()), t2_end(b.end()) {} 
-        
-        iterator(T1& a, T2& b, bool) 
-        : t1_begin(a.end()), t1_end(a.end()),
-          t2_begin(b.end()), t2_end(b.end()) {} 
-          
-        iterator& operator++() { 
-         ++ t1_begin;
-         ++ t2_begin;
-        return *this; 
-        }
+            decltype(_TypeA.begin()) t1_begin;
+            decltype(_TypeA.end()) t1_end;
 
-        MyPair<decltype(*t1_begin),decltype(*t2_begin)> operator*() { 
-        return MyPair<decltype(*t1_begin),decltype(*t2_begin)> (*t1_begin,*t2_begin);
-        } 
+            decltype(_TypeB.begin()) t2_begin;
+            decltype(_TypeB.end()) t2_end;
 
-        bool operator != (const iterator& iter) {
-            if( t1_begin != iter.t1_begin && t2_begin != iter.t2_begin) return true;
-            else return false;
-            // == (Negation)
-            // t1_begin == iter.t1_begin || t2_begin == iter.t2_begin
-        }       
-     }; // End Struct Iterator
-        
-        iterator begin() { return iterator(a,b); }
-        iterator end() { return iterator(a,b,false); }
+            public:   
+            iterator(T& _TypeA, U& _TypeB) 
+            : t1_begin(_TypeA.begin()), t1_end(_TypeA.end()),
+            t2_begin(_TypeB.begin()), t2_end(_TypeB.end()) {} 
+            
+            iterator(T& _TypeA, U& _TypeB, bool) 
+            : t1_begin(_TypeA.end()), t1_end(_TypeA.end()),
+            t2_begin(_TypeB.end()), t2_end(_TypeB.end()) {} 
+            
+            iterator& operator++(){
+                ++ t1_begin;
+                ++ t2_begin;
+                return *this; 
+            }
+
+            MyPair<decltype(*t1_begin),decltype(*t2_begin)> operator*() { 
+            return MyPair<decltype(*t1_begin),decltype(*t2_begin)> (*t1_begin,*t2_begin);
+            } 
+
+            bool operator!=(const iterator& iter) {
+                if( t1_begin != iter.t1_begin && t2_begin != iter.t2_begin) return true;
+                else return false;
+            }
+        };
+        iterator begin() { return iterator(_TypeA,_TypeB); }
+        iterator end() { return iterator(_TypeA,_TypeB,false); }
 
     };
-} // end namespace
-
-
-#endif
+}
