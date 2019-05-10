4ae179adfe766928777c87377e659e99de918d59
Or Abuhtzura <44296674+orabu103@users.noreply.github.com>
2019-05-10 12:18:28 +0300

    Update chain.hpp

--- chain.hpp
+++ chain.hpp
@@ -1,56 +1,48 @@
-#ifndef CHAIN_HPP
-#define CHAIN_HPP
-
+#pragma once
+using namespace std;
 #include <iostream>
 
-namespace itertools {
-template <typename T1, typename T2> class chain {
-        private:
-        T1 a; // range(5,8) for Example
-        T2 b; // range(9,10) for Example
-        
+namespace itertools{
+    template <typename T,typename U >
+
+    class chain{
         public:
-        chain(const T1& r1,const T2& r2) : a(r1), b(r2) {}
+        T _TypeA;
+        U _TypeB;
 
-       struct iterator {        
-        iterator(T1& a, T2& b) 
-        : t1_begin(a.begin()), t1_end(a.end()),
-          t2_begin(b.begin()), t2_end(b.end()) {} 
+        chain(const T& TypeA,const U& TypeB): _TypeA(TypeA), _TypeB(TypeB) {}
         
-        iterator(T1& a, T2& b, bool) 
-        : t1_begin(a.end()), t1_end(a.end()),
-          t2_begin(b.end()), t2_end(b.end()) {} 
-          
-        iterator& operator++() { 
-        if(t1_begin != t1_end) ++ t1_begin;
-        else ++ t2_begin;
-        return *this; 
-        }
+        struct iterator {
+            decltype(_TypeA.begin()) t1_begin;
+            decltype(_TypeA.end()) t1_end;
 
-        auto operator*() { 
-        if(t1_begin != t1_end) return *t1_begin;
-        else return *t2_begin;
-        } 
+            decltype(_TypeB.begin()) t2_begin;
+            decltype(_TypeB.end()) t2_end;
 
-        bool operator!=(const iterator& iter) {
-            if(t1_begin != iter.t1_begin || t2_begin != iter.t2_begin) return true;
-            else return false;
-            // == (Negation)
-            // t1_begin == iter.t1_begin && t2_begin == iter.t2_begin
-        }
-       
-        private:
-        decltype(a.begin()) t1_begin;
-        decltype(a.end()) t1_end;
-
-        decltype(b.begin()) t2_begin;
-        decltype(b.end()) t2_end;
-       
-     }; // End Struct Iterator
-        
-        iterator begin() { return iterator(a,b); }
-        iterator end() { return iterator(a,b,false); }
+            iterator(T& _TypeA, U& _TypeB) 
+            : t1_begin(_TypeA.begin()), t1_end(_TypeA.end()),
+            t2_begin(_TypeB.begin()), t2_end(_TypeB.end()) {} 
+            
+            iterator(T& _TypeA, U& _TypeB, bool) 
+            : t1_begin(_TypeA.end()), t1_end(_TypeA.end()),
+            t2_begin(_TypeB.end()), t2_end(_TypeB.end()) {} 
+            
+            iterator& operator++(){
+                if(t1_begin != t1_end) ++ t1_begin;
+                else ++ t2_begin;
+                return *this; 
+            }
+            auto operator*() { 
+                if(t1_begin != t1_end) return *t1_begin;
+                else return *t2_begin;
+            }
+            bool operator!=(const iterator& iter) {
+                if(t1_begin != iter.t1_begin || t2_begin != iter.t2_begin) return true;
+                else return false;
+            }
+        };
+        iterator begin() { return iterator(_TypeA,_TypeB); }
+        iterator end() { return iterator(_TypeA,_TypeB,false); }
     };
 }
 
-#endif
