950ad5a14cd1caca33c26c2e4428312ad78372f5
Or Abuhtzura <44296674+orabu103@users.noreply.github.com>
2019-05-10 13:01:22 +0300

    Update product.hpp

--- product.hpp
+++ product.hpp
@@ -1,77 +1,66 @@
-
-#ifndef PRODUCT_HPP
-#define PRODUCT_HPP
-
+#pragma once
+using namespace std;
 #include <iostream>
 
-namespace itertools {
-
-template <typename T1, typename T2> class product {
-        private:
-        T1 a; // range(5,8) for Example
-        T2 b; // range(9,10) for Example
-        
+namespace itertools{
+    template<typename T , typename U>
+    
+    class product{
         public:
-        product(const T1& r1,const T2& r2) : a(r1), b(r2) {}
+        T _TypeA ;
+        U _TypeB;
 
-    class iterator {  
-      
-      template <typename V, typename K>
-      class MyPair {
-      V v;
-      K k;
-      public:
-      MyPair(const V& v, const K& k) : v(v), k(k) {}
-      MyPair() {}
+        product(const T& TypeA , const U& TypeB): _TypeA(TypeA) , _TypeB(TypeB) {}
 
-      friend std::ostream& operator<<(std::ostream& os,const MyPair<V,K>& pair)
-      {
-        return os << pair.v << "," << pair.k;
-      }
-      }; // End Class MyPair
-        decltype(a.begin()) t1_begin;
-        decltype(a.end()) t1_end;
-
-        decltype(b.begin()) running_pointer;
-        decltype(b.begin()) t2_begin;
-        decltype(b.end()) t2_end;
-       
-        public:      
-        iterator(T1& a, T2& b) 
-        : t1_begin(a.begin()), t1_end(a.end()),
-          running_pointer(b.begin()), t2_begin(b.begin()), t2_end(b.end()) {} 
         
-        iterator(T1& a, T2& b, bool) 
-        : t1_begin(a.end()), t1_end(a.end()),
-          running_pointer(b.end()), t2_begin(b.end()), t2_end(b.end()) {} 
-          
-        iterator& operator++() {
-        if( running_pointer != t2_end - 1 ) ++ running_pointer;
-        else
-        {
-            ++ t1_begin;
-            running_pointer = t2_begin;
-        }
-        return *this; 
-        }
+        class iterator {
+            template <typename V,typename K >
+            class MyPair {
+                V v;
+                K k;
+                public:
+                MyPair(const V& v, const K& k) : v(v), k(k) {}
+                MyPair() {}
 
-        MyPair<decltype(*t1_begin),decltype(*t2_begin)> operator*() { 
-        return MyPair<decltype(*t1_begin),decltype(*running_pointer)> (*t1_begin,*running_pointer);
-        } 
+                friend std::ostream& operator<<(std::ostream& os,const MyPair<V,K>& pair)
+                {
+                    return os << pair.v << "," << pair.k;
+                }
+            }; // End Class MyPair
 
-        bool operator != (const iterator& iter) {
-            if( t1_begin != iter.t1_begin ) return true;
-            else return false;
-            // == (Negation)
-            // t1_begin == iter.t1_begin || t2_begin == iter.t2_begin
-        }  
-     }; // End class Iterator
-        
-        iterator begin() { return iterator(a,b); }
-        iterator end() { return iterator(a,b,false); }
+            decltype(_TypeA.begin()) t1_begin;
+            decltype(_TypeA.end()) t1_end;
 
+            decltype(_TypeB.begin()) temp;
+            decltype(_TypeB.begin()) t2_begin;
+            decltype(_TypeB.end()) t2_end;
+
+            public:   
+            iterator(T& _TypeA, U& _TypeB) 
+            : t1_begin(_TypeA.begin()), t1_end(_TypeA.end()),
+            t2_begin(_TypeB.begin()), t2_end(_TypeB.end()),temp(_TypeB.begin()) {} 
+            
+            iterator(T& _TypeA, U& _TypeB, bool) 
+            : t1_begin(_TypeA.end()), t1_end(_TypeA.end()),
+            t2_begin(_TypeB.end()), t2_end(_TypeB.end()),temp(_TypeB.begin()) {} 
+            
+            iterator& operator++(){
+                if(temp == t2_end-1){ ++t1_begin; temp = t2_begin; } 
+                else
+                ++temp;
+                return *this; 
+            }
+            MyPair<decltype(*t1_begin),decltype(*temp)> operator*() { 
+            return MyPair<decltype(*t1_begin),decltype(*temp)> (*t1_begin,*temp);
+            } 
+
+            bool operator!=(const iterator& iter) {
+                if( t1_begin != iter.t1_begin ) return true;
+                else return false;
+            }
+        };
+        iterator begin() { return iterator(_TypeA,_TypeB); }
+        iterator end() { return iterator(_TypeA,_TypeB,false); }
     };
-} // end namespace
-
-
-#endif
+}
+        
