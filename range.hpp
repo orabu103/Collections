8130f3e59f489d0509e5b4efde09b4b1b9435cd9
Or Abuhtzura <44296674+orabu103@users.noreply.github.com>
2019-05-25 14:20:11 +0300

    Update range.hpp

--- range.hpp
+++ range.hpp
@@ -1,26 +1,29 @@
-#pragma once
-using namespace std;
+#ifndef RANGE_HPP
+#define RANGE_HPP
+
 #include <iostream>
 
-
-namespace itertools{
-    template<typename T>
-    class range{
-    
+namespace itertools {
+template <typename T> class range {
+        private:
+        T a; // begin Point
+        T b; // End Point
         public:
-        T _begin;
-        T _end;
-
-        range(const T& begin,const T& end): _begin(begin), _end(end) {}
+        range(T begin,T end) : a(begin), b(end) {}
+        struct const_iterator {
+        const_iterator(T p) : type(p) {} 
+        const_iterator& operator++(){ ++type; return *this; } 
+        const T& operator*() const { return type; } 
+        bool operator!=(const const_iterator& iter) const { return type != iter.type; }
+       
+        private:
+        T type; 
+       
+     }; // End Struct const_iterator
         
-        struct iterator {
-            T type;  
-            iterator(const T& p) :type(p) {} 
-            iterator& operator++(){ ++type; return *this; } 
-            T& operator*() { return type; } 
-            bool operator!=(const iterator& iter) { return type != iter.type; }  
-        };
-        iterator begin(){ return _begin; }
-        iterator end(){ return _end; }
+        const_iterator begin() const { return const_iterator(a); }
+        const_iterator end() const { return const_iterator(b); }
     };
-}
+} // End Namespace 'itertools'
+
+#endif
